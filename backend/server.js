706effaeed811859a109ddd56ab1f2745f7b7a71
mli254 <mli254@ucsc.edu>
2026-06-29 18:31:21 -0700

    converted to import syntax and ensured connection was possible

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
-const mysql = require('mysql2');
-const express = require('express');
+import dotenv from "dotenv/config"; // needs to be the very first import
+import mysql from 'mysql2/promise';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -11,4 +14,17 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database!');
 });
