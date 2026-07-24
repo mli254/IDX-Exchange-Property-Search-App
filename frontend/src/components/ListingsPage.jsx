0db7210d69108041202bd58ba1ecc80cf3b06e1f
mli254 <mli254@ucsc.edu>
2026-07-23 21:33:18 -0700

    putting in placeholder components and files

---
+++ frontend/src/components/ListingsPage.jsx
@@ -0,0 +1,10 @@
+import PropertyCard from './PropertyCard'
+
+export default function ListingsPage() {
+    return (<>
+    <div className="text-center">
+        placeholder for ListingsPage
+        <PropertyCard/>
+    </div>
+    </>);
+}
---
+++ frontend/src/components/PropertyCard.jsx
@@ -0,0 +1,7 @@
+export default function PropertyCard() {
+    return (<>
+    <div class="text-center box box-border border-black border-2">
+        placeholder for PropertyCard
+    </div>
+    </>);
+}
