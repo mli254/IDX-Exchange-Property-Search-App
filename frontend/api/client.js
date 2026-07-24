5c08fd56eedd862b6ebd16f10b39da3a79376818
mli254 <mli254@ucsc.edu>
2026-07-23 20:30:55 -0700

    changed folder structure

---
+++ frontend/api/client.js
@@ -0,0 +1,15 @@
+export const fetchProperties = async (params) => {
+    try {
+        console.log(params);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const fetchPropertyDetail = async (id) => {
+    try {
+        console.log(id);
+    } catch (e) {
+        console.log(e);
+    }
+}
